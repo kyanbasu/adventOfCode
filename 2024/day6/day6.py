bd3d4ca3e28102a4918a32ab73bd602604e38b4a
bnm <bartekbata1302@gmail.com>
2024-12-06 18:13:02 +0100

    part 2 only for few cases idk

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -70,14 +70,101 @@ def part1(mat):
 
     return places
 
+def part2(mat):
+    if mat[-1] == '':
+        mat.pop()
 
+
+    guard_pos = ()
+    guard_direction = 0
+    directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    guard_icons = ['^', '>', 'v', '<'] # also for visualization
+
+    # get guard position
+    for y in range(len(mat)):
+        match = next( re.finditer(r'\^', mat[y]), None )
+        if match is not None:
+            guard_pos = (y, match.span()[0])
+            lst = list(mat[y])
+            lst[guard_pos[1]] = '.'
+            mat[y] = ''.join(lst)
+
+    last_redirects = []
+
+    ans = 0
+
+    while True:
+
+        pos = guard_pos
+        dr = directions[guard_direction]
+
+        if pos[0] + dr[0] < 0 or pos[0] + dr[0] >= len(mat) or pos[1] + dr[1] < 0 or pos[1] + dr[1] >= len(mat[0]):
+            break
+
+        if mat[pos[0] + dr[0]][pos[1] + dr[1]] == '#':
+            # redirects for part 2
+            if len(last_redirects) > 2:
+                match guard_direction:
+                    case 0: #going up
+                        if last_redirects[0][0] > pos[0]:
+                            ans += 1
+                    case 1: #going right
+                        if last_redirects[0][1] < pos[1]:
+                            ans += 1
+                    case 2:  # going up
+                        if last_redirects[0][0] < pos[0]:
+                            ans += 1
+                    case 3: #going left
+                        if last_redirects[0][1] > pos[1]:
+                            ans += 1
+                    case _:
+                        pass
+                last_redirects.pop(0)
+            last_redirects.append(pos)
+
+            # rotate guard
+            guard_direction += 1
+            if guard_direction > 3:
+                guard_direction = 0
+            dr = directions[guard_direction]
+
+
+        guard_pos = (guard_pos[0] + dr[0], guard_pos[1] + dr[1])
+
+        os.system('cls')
+        #prints mat state
+        for y in range(len(mat)):
+            for x in range(len(mat[y])):
+                if (y, x) != guard_pos:
+                    print(mat[y][x], end='')
+                else:
+                    print(guard_icons[guard_direction], end='')
+
+            print('\t', end='')
+
+            for x in range(len(mat[y])):
+                if (y, x) in last_redirects:
+                    print('#', end='')
+                else:
+                    print('.', end='')
+
+            print('')
+
+        print(ans)
+
+        sleep(.5)
+
+    return ans
 
 if __name__ == "__main__":
     e = open("example.txt").read().split('\n')
-    ea = part1(e)
-    assert ea == 41
+    #_in = open("input.txt").read().split('\n')
 
-    _in = open("input.txt").read().split('\n')
+    """e1 = part1(e)
+    assert e1 == 41
+
     p1 = part1(_in)
+    print("distinct positions on map: " + str(p1))"""
 
-    print("distinct positions on map: " + str(p1))
+    e2 = part2(e)
+    assert e2 == 6
